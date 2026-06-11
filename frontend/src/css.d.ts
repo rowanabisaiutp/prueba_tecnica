0cdce0627e77e038a1fcd178cf4f8a50f39e5df1
rowanabisaiutp <a.rowan.ojeda@utponiente.edu.mx>
2026-06-10 19:55:33 -0600

    ci integration buld test

---
+++ frontend/src/css.d.ts
@@ -0,0 +1,11 @@
+// CSS files
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+// CSS modules
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
